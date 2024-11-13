fb907246d39aef89d3eed8c02c82dd210bd3b44a
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-11-13 10:59:42 +0700

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,13 @@
+public class Customer {
+    private String address, code, name;
+    public Customer(String address, String code, String name) {
+        this.address = address;
+        this.code = code;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Code: " + code + ", Address: " + address;
+    }
+}
