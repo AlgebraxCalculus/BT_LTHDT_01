ddbee23028923ee27de39bdc35689c59a5849e2b
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-11-13 10:57:51 +0700

    Add files via upload

---
+++ BÀI 1 - TÍNH LƯƠNG NHÂN VIÊN/CalculateSalary.java
@@ -0,0 +1,11 @@
+public class CalculateSalary {
+
+    public static void main(String[] args) {
+        IEmployee p1 = new PartTimeEmployee(13, "Nguyen Van A", 15000);
+        IEmployee p2 = new FullTimeEmployee("Tran Van B", 78000);
+        System.out.println("Salary: " + p1.calculateSalary());
+        System.out.println("Name: " + p1.getName());
+        System.out.println("Salary: " + p2.calculateSalary());
+        System.out.println("Name: " + p2.getName());
+    }
+}
