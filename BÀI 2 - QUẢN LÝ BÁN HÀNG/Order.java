8e2b2c68e507e429679e2d2520b9a62be190206e
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-11-13 11:00:31 +0700

    Add files via upload

---
+++ BÀI 2 - QUẢN LÝ BÁN HÀNG/Order.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Currency;
+
+public class Order {
+    private Customer customer;
+    private ArrayList<OrderLine> orderLine;
+    private Currency total;
+
+    public Order(Customer customer) {
+        this.customer = customer;
+        this.orderLine = new ArrayList<>();
+    }
+
+    public Currency getTotal() {
+        return total;
+    }
+
+    public void setTotal(Currency total) {
+        this.total = total;
+    }
+
+    public OrderLine addLine(OrderLine line) {
+        orderLine.add(line);
+        return line;
+    }
+
+    public void removeLine(OrderLine line) {
+        orderLine.remove(line);
+    }
+
+    public void inOrder() {
+        System.out.println("Order Info:");
+        System.out.println("Customer: " + customer);
+        System.out.println("Total: " + total);
+        System.out.println("Order Lines:");
+        for (OrderLine line : orderLine) {
+            System.out.println(" - Value: " + line.getValue());
+        }
+    }
+}
---
+++ BÀI 2 - QUẢN LÝ BÁN HÀNG/OrderLine.java
@@ -0,0 +1,13 @@
+import java.util.Currency;
+
+public class OrderLine {
+    private Currency value;
+
+    public OrderLine(Currency value) {
+        this.value = value;
+    }
+
+    public Currency getValue() {
+        return value;
+    }
+}
